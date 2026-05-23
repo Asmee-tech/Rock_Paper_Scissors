0044a0c0df1b2c670a067758836ca7394dc9b026
Asmee-tech <asmee.khimani@gmail.com>
2026-05-23 11:00:02 +0200

    Refactor GUI layout for Rock Paper Scissors
    Reorganize layout of labels and buttons in the GUI

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -8,29 +8,45 @@ screen.title("Rock paper scissors")
 screen.geometry("700x300")
 
 lab1=Label(screen,text="Rock Paper Scissors",font=font.Font(size=20),fg="black")
-lab2=Label(screen,text="Let's start the game...",fg="green",font=font.Font(size=15))
-frame1=Frame(screen)
-lab3=Label(frame1,text="Your Options:",fg="black",pady=8,font=font.Font(size=12))    
-rock=Button(frame1,text="Rock",pady=5,font=font.Font(size=20),fg="black",width=5,bd=0,bg="pink")
-paper=Button(frame1,text="Paper",pady=5,font=font.Font(size=20),fg="black",width=5,bd=0,bg="purple")
-scissors=Button(frame1,text="Scissors",pady=5,font=font.Font(size=20),fg="black",width=6,bd=0,bg="blue")
-score=Label(frame1,text="Score:",font=font.Font(size=12))
-u=Label(frame1,text="You selected:")
-comp=Label(frame1,text="Computer selected:")
-us=Label(frame1,text="Your score:")
-cs=Label(frame1,text="Computer selected:")
-
 lab1.pack()
+
+lab2=Label(screen,text="Let's start the game...",fg="green",font=font.Font(size=15))
 lab2.pack()
+
+frame1=Frame(screen)
 frame1.pack()
+
+lab3=Label(frame1,text="Your Options:",fg="black",pady=8,font=font.Font(size=12))
 lab3.grid(row=0,column=0,pady=8)
+    
+rock=Button(frame1,text="Rock",pady=5,font=font.Font(size=20),fg="black",width=5,bd=0,bg="pink")
 rock.grid(row=1,column=1,padx=8,pady=5)
+
+paper=Button(frame1,text="Paper",pady=5,font=font.Font(size=20),fg="black",width=5,bd=0,bg="purple")
 paper.grid(row=1,column=2,padx=8,pady=5)
+
+scissors=Button(frame1,text="Scissors",pady=5,font=font.Font(size=20),fg="black",width=6,bd=0,bg="blue")
 scissors.grid(row=1,column=3,padx=8,pady=5)
+
+score=Label(frame1,text="Score:",font=font.Font(size=12))
 score.grid(row=2,column=0,padx=8,pady=5)
+
+u=Label(frame1,text="You selected:")
 u.grid(row=2,column=1,padx=8,pady=5)
+
+comp=Label(frame1,text="Computer selected:")
 comp.grid(row=3,column=1,padx=8,pady=5)
+
+us=Label(frame1,text="Your score:")
 us.grid(row=2,column=2,padx=8,pady=5)
+
+cs=Label(frame1,text="Computer score:")
 cs.grid(row=3,column=2,padx=8,pady=5)
+
+us=0
+cs=0
+def playerchoice(player_input):
+    u.config(text="You selected:"+player_input)
+def compchoice()
 
 screen.mainloop()
