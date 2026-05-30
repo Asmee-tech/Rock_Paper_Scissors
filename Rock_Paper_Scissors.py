a81530d0d87781e068f16b1c7890712ac4ce0fab
Asmee-tech <asmee.khimani@gmail.com>
2026-05-30 10:59:30 +0200

    Enhance game functionality and fix button commands
    Refactor Rock Paper Scissors game logic and UI elements.

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -7,6 +7,44 @@ screen=Tk()
 screen.title("Rock paper scissors")
 screen.geometry("700x300")
 
+ys=0
+ls=0
+def compchoice():
+    comp_input=random.choice(["Rock","Paper","Scissors"])
+    return comp_input
+
+def tie():
+    global ys, ls
+    lab2.config(text="Tie!")
+    us.config(text="Your score:"+str(ys))
+    cs.config(text="Computer score:"+str(ls))
+
+def pw():
+    global ys, ls
+    lab2.config(text="You win!")
+    us+=1
+    us.config(text="Your score:"+str(ys))
+    cs.config(text="Computer score:"+str(ls))
+    
+def cw():
+    global ys, ls
+    lab2.config(text="Computer wins!")
+    cs+=1
+    us.config(text="Your score:"+str(ys))
+    cs.config(text="Computer score:"+str(ls))
+
+def playerchoice(player_input):
+    comp_input=compchoice()
+    u.config(text="You selected:"+player_input)
+    comp.config(text="Computer selected:"+comp_input)
+     
+    if comp_input==player_input:       
+        tie()
+    elif comp_input=="rock" and player_input=="paper" or comp_input=="scissors" and player_input=="rock" or comp_input=="paper" and player_input=="scissors":
+        pw()
+    else:
+        cw()
+
 lab1=Label(screen,text="Rock Paper Scissors",font=font.Font(size=20),fg="black")
 lab1.pack()
 
@@ -19,13 +57,13 @@ frame1.pack()
 lab3=Label(frame1,text="Your Options:",fg="black",pady=8,font=font.Font(size=12))
 lab3.grid(row=0,column=0,pady=8)
     
-rock=Button(frame1,text="Rock",pady=5,font=font.Font(size=20),fg="black",width=5,bd=0,bg="pink")
+rock=Button(frame1,text="Rock",pady=5,font=font.Font(size=20),fg="black",width=5,bd=0,bg="pink", command=playerchoice("Rock"))
 rock.grid(row=1,column=1,padx=8,pady=5)
 
-paper=Button(frame1,text="Paper",pady=5,font=font.Font(size=20),fg="black",width=5,bd=0,bg="purple")
+paper=Button(frame1,text="Paper",pady=5,font=font.Font(size=20),fg="black",width=5,bd=0,bg="purple",command=playerchoice("Paper"))
 paper.grid(row=1,column=2,padx=8,pady=5)
 
-scissors=Button(frame1,text="Scissors",pady=5,font=font.Font(size=20),fg="black",width=6,bd=0,bg="blue")
+scissors=Button(frame1,text="Scissors",pady=5,font=font.Font(size=20),fg="black",width=6,bd=0,bg="blue",command=playerchoice("Scissors"))
 scissors.grid(row=1,column=3,padx=8,pady=5)
 
 score=Label(frame1,text="Score:",font=font.Font(size=12))
@@ -43,10 +81,5 @@ us.grid(row=2,column=2,padx=8,pady=5)
 cs=Label(frame1,text="Computer score:")
 cs.grid(row=3,column=2,padx=8,pady=5)
 
-us=0
-cs=0
-def playerchoice(player_input):
-    u.config(text="You selected:"+player_input)
-def compchoice()
 
 screen.mainloop()
